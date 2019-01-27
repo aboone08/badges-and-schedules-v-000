4dc570b88d7b8aca579dfed87e8e369c99950ada
Andranique Boone <andranique.boone@gmail.com>
2019-01-27 03:42:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def badge_maker(speaker)
+  puts "Hello, my name is #{speaker}."
+end
+
+def batch_badge_creator(speakers)
+  speakers.collect do |speaker|
+    badge_maker(speaker)
+  end
+end
+
+def assign_rooms
+
+end
+
+def printer
+  puts batch_badge_creator
+  puts assign_rooms
+end
